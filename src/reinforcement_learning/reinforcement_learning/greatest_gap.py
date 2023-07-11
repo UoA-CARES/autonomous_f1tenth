50451a9f27df92a59d11efb1391918ab870ff376
emilysteiner71 <emilysteiner71@gmail.com>
2023-07-11 15:39:22 +1200

    greatest_gap file now runs, chooses actions random

--- src/reinforcement_learning/reinforcement_learning/greatest_gap.py
+++ src/reinforcement_learning/reinforcement_learning/greatest_gap.py
@@ -8,7 +8,6 @@ from cares_reinforcement_learning.algorithm.policy import TD3
 from cares_reinforcement_learning.util import helpers as hlp
 from cares_reinforcement_learning.memory import MemoryBuffer
 from cares_reinforcement_learning.util import Record
-from cares_reinforcement_learning.networks.TD3 import Actor, Critic
 
 from environments.CarGoalEnvironment import CarGoalEnvironment
 from environments.CarWallEnvironment import CarWallEnvironment
@@ -79,20 +78,7 @@ def main():
             env = CarTrack2Environment('f1tenth', step_length=STEP_LENGTH, max_steps=MAX_STEPS, reward_range=REWARD_RANGE, collision_range=COLLISION_RANGE)
         case _:
             env = CarGoalEnvironment('f1tenth', step_length=STEP_LENGTH, max_steps=MAX_STEPS, reward_range=REWARD_RANGE)
-    
-    actor = Actor(observation_size=env.OBSERVATION_SIZE, num_actions=env.ACTION_NUM, learning_rate=ACTOR_LR)
-    critic = Critic(observation_size=env.OBSERVATION_SIZE, num_actions=env.ACTION_NUM, learning_rate=CRITIC_LR)
-
-    agent = TD3(
-        actor_network=actor,
-        critic_network=critic,
-        gamma=GAMMA,
-        tau=TAU,
-        action_num=env.ACTION_NUM,
-        device=DEVICE
-    )
-
-    networks = {'actor': actor, 'critic': critic}
+   
     config = {
         'environment': ENVIRONMENT,
         'max_steps_training':MAX_STEPS_TRAINING,
@@ -110,9 +96,32 @@ def main():
         'reward_range': REWARD_RANGE,
         'collision_range': COLLISION_RANGE
     }
-    record = Record(networks=networks, checkpoint_freq=100, config=config)
+    record = Record(checkpoint_freq=100, config=config)
 
-    train(env=env, agent=agent, record=record)
+    greatestGap(env)
+
+def greatestGap(env):
+    state, _ = env.reset()
+
+    episode_timesteps = 0
+    episode_reward    = 0
+    episode_num       = 0
+
+    for total_step_counter in range(int(MAX_STEPS_TRAINING)):
+        episode_timesteps += 1
+        action_env = np.asarray([random.uniform(env.MIN_ACTIONS[0], env.MAX_ACTIONS[0]), random.uniform(env.MIN_ACTIONS[1], env.MAX_ACTIONS[1])]) # action range the env uses [e.g. -2 , 2 for pendulum]
+        action = hlp.normalize(action_env, env.MAX_ACTIONS, env.MIN_ACTIONS)
+        next_state, reward, done, truncated, info = env.step(action_env)
+        state = next_state
+
+        if done or truncated:
+            # Reset environment
+            state, _ = env.reset()
+            episode_reward    = 0
+            episode_timesteps = 0
+            episode_num += 1
+
+
 
 def train(env, agent: TD3, record: Record):
     
