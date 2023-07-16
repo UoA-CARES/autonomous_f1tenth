9d0fd02247a89a1b8e4faa518b479bd63e89b201
emilysteiner71 <emilysteiner71@gmail.com>
2023-07-17 10:11:42 +1200

    input info

--- src/environments/environments/follow_the_gap.py
+++ src/environments/environments/follow_the_gap.py
@@ -7,6 +7,9 @@ class FollowTheGapNode(Node):
         super().__init__('follow_the_gap')
 
     def select_action(state):
+        # Current x: state[0], current y: state[1], current z: state[2], orientation x: state[3], orientation y: state[4], orientation z: state[5]
+        # linear vel x: state[6], angular vel z: state[7], LIDAR points 1-10: state[8-17]
+        
         lin = 5
         ang = 0
         action = np.asarray([lin, ang])
