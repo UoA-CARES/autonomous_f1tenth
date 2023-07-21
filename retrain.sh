3278e9c2fd8bb4d5151fc043e4b9a7808253ad3f
Jack17432 <Chrisgraham908@gmail.com>
2023-07-21 20:46:49 +1200

    Update the paths after each restart

--- retrain.sh
+++ retrain.sh
@@ -9,8 +9,10 @@ trap cleanup SIGINT SIGTERM
 
 # Time limit for training
 if [ -z "$1" ]; then
-  echo "Please enter the time limit for training, usage: ./retrain.sh <time_limit> [<partition_number>]"
-  echo "Eg: 10s, 6h -> usage: ./retrain.sh 10s"
+  echo "Please enter the time limit for training"
+  echo "Usage: ./retrain.sh <time_limit> [<partition_number>]"
+  echo "Eg: 10s, 6h"
+  echo "Usage: ./retrain.sh 10s"
   exit 1
 fi
 
@@ -29,12 +31,20 @@ while true; do
 	timeout "$1" gz sim -g &
 	timeout "$1" ros2 launch reinforcement_learning train.launch.py
 
+  # Get the latest folder in rl_logs
+	latest_folder=$(ls -lt ./rl_logs | grep '^d' | head -n 1 | awk '{print $NF}')
+
 	# Set the new paths for actor_path and critic_path
-	new_actor_path=""
-	new_critic_path=""
+	new_actor_path="rl_logs/$latest_folder/models/actor_checkpoint.pht"
+	new_critic_path="rl_logs/$latest_folder/models/critic_checkpoint.pht"
+
+  # Check if the new paths exist
+	if [ ! -e "$new_actor_path" ] || [ ! -e "$new_critic_path" ]; then
+    echo "Error: $new_actor_path or $new_critic_path does not exist"
+    exit 1
+  fi
 
 	# Use sed to update the actor_path and critic_path in the YAML file
 	sed -i "s#actor_path: '.*'#actor_path: '$new_actor_path'#g" src/reinforcement_learning/config/train.yaml
 	sed -i "s#critic_path: '.*'#critic_path: '$new_critic_path'#g" src/reinforcement_learning/config/train.yaml
 done
-
