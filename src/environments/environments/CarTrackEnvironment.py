0d6887ced8427dfe0eb6451c9dac6bf729f85d9b
retinfai <aferetipama@gmail.com>
2023-09-01 18:10:38 +1200

    Modify lap reward

--- src/environments/environments/CarTrackEnvironment.py
+++ src/environments/environments/CarTrackEnvironment.py
@@ -210,7 +210,7 @@ class CarTrackEnvironment(F1tenthEnvironment):
         if self.steps_since_last_goal >= 10:
             reward -= 10
 
-        if self.goals_reached == len(self.all_goals) * self.laps_to_run:
+        if self.goals_reached != 0 and self.goals_reached % len(self.all_goals) == 0:
             reward += 100
         
         if has_collided(next_state[8:], self.COLLISION_RANGE) or has_flipped_over(next_state[2:6]):
