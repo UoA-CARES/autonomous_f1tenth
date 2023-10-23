1d7c56a8d17b4121d2ffcf968f158fb7ad4a8833
retinfai <aferetipama@gmail.com>
2023-10-24 08:07:04 +1300

    feat: integrate Network and Memory factory

--- src/reinforcement_learning/reinforcement_learning/updated_train.py
+++ src/reinforcement_learning/reinforcement_learning/updated_train.py
@@ -11,6 +11,7 @@ from cares_reinforcement_learning.memory import MemoryBuffer
 from cares_reinforcement_learning.util.Record import Record
 from cares_reinforcement_learning.util.NetworkFactory import NetworkFactory
 from cares_reinforcement_learning.util import helpers as hlp
+import cares_reinforcement_learning.util.configurations as cfg
 
 from .parse_args import parse_args
 from .EnvironmentFactory import EnvironmentFactory
@@ -26,8 +27,6 @@ def main():
     np.random.seed(algorithm_config['seed'])
     random.seed(algorithm_config['seed'])
 
-    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
     print(
         f'Environment Config: ------------------------------------- \n'
         f'{yaml.dump(env_config, default_flow_style=False)} \n'
@@ -38,5 +37,29 @@ def main():
     )
 
     env_factory = EnvironmentFactory()
+    network_factory = NetworkFactory()
+
+    match network_config['algorithm']:
+        case 'PPO':
+            config = cfg.PPOConfig(**network_config)
+        case 'DDPG':
+            config = cfg.DDPGConfig(**network_config)
+        case 'SAC':
+            config = cfg.SACConfig(**network_config)
+        case 'TD3':
+            config = cfg.TD3Config(**network_config)
+        case _:
+            raise Exception(f'Algorithm {network_config["algorithm"]} not implemented')
+
+
     env = env_factory.create(env_config['environment'], env_config)
+    agent = network_factory.create_network(env.OBSERVATION_SIZE, env.ACTION_NUM, config=config)
+    memory = MemoryBuffer(algorithm_config['buffer_size'], env.OBSERVATION_SIZE, env.ACTION_NUM)
+
+    # TODO: Load Actor and Critic if passed. Only load if both are passed
+
+
+
+
+
 
