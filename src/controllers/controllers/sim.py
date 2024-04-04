0de77a170b0b936c230ab8dd50309d2e4a3da1b2
emilysteiner71 <emilysteiner71@gmail.com>
2024-04-05 10:36:07 +1300

    Traditional testing environment working

---
+++ src/controllers/controllers/sim.py
@@ -0,0 +1,38 @@
+import random
+from datetime import datetime
+import yaml
+
+import rclpy
+import torch
+
+from cares_reinforcement_learning.util.network_factory import NetworkFactory
+import cares_reinforcement_learning.util.configurations as cfg
+
+from reinforcement_learning.parse_args import parse_args
+from reinforcement_learning.EnvironmentFactory import EnvironmentFactory
+#from .training_loops import off_policy_evaluate, ppo_evaluate
+
+
+def main():
+    rclpy.init()
+
+    env_config, algorithm_config, network_config, rest = parse_args()
+
+    print(
+        f'Environment Config: ------------------------------------- \n'
+        f'{yaml.dump(env_config, default_flow_style=False)} \n'
+        f'Algorithm Config: ------------------------------------- \n'
+        f'{yaml.dump(algorithm_config, default_flow_style=False)} \n'
+        f'Network Config: ------------------------------------- \n'
+        f'{yaml.dump(network_config, default_flow_style=False)} \n'
+    )
+
+    env_factory = EnvironmentFactory()
+
+    env = env_factory.create(env_config['environment'], env_config)
+
+    state, _ = env.reset()
+    
+
+if __name__ == '__main__':
+    main()
---
+++ src/controllers/launch/sim.launch.py
@@ -0,0 +1,75 @@
+import os
+from ament_index_python import get_package_share_directory
+from launch_ros.actions import Node, SetParameter
+from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.substitutions import TextSubstitution
+import yaml
+
+env_launch = {
+    'CarGoal': 'cargoal',
+    'CarWall': 'carwall',
+    'CarBlock': 'carblock',
+    'CarTrack': 'cartrack',
+    'CarBeat': 'carbeat',
+}
+
+alg_launch = {
+    'ftg': 'ftg',
+    'rl': 'rl',
+}
+
+def generate_launch_description():
+    pkg_f1tenth_description = get_package_share_directory('f1tenth_description')
+    pkg_environments = get_package_share_directory('environments')
+    pkg_controllers = get_package_share_directory('controllers')
+
+    config_path = os.path.join(
+        pkg_controllers,
+        'sim.yaml'
+    )
+
+    config = yaml.load(open(config_path), Loader=yaml.Loader)
+    env = config['sim']['ros__parameters']['environment']
+    alg = config['sim']['ros__parameters']['algorithm']
+
+    environment =  IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+            os.path.join(pkg_environments, f'{env_launch[env]}.launch.py')),
+        launch_arguments={
+            'track': TextSubstitution(text=str(config['sim']['ros__parameters']['track'])),
+            'car_name': TextSubstitution(text=str(config['sim']['ros__parameters']['car_name']) if 'car_name' in config['sim']['ros__parameters'] else 'f1tenth'),
+            'car_one': TextSubstitution(text=str(config['sim']['ros__parameters']['car_name']) if 'car_name' in config['sim']['ros__parameters'] else 'f1tenth'),
+            #'car_two': TextSubstitution(text=str(config['sim']['ros__parameters']['ftg_car_name']) if 'ftg_car_name' in config['sim']['ros__parameters'] else 'ftg_car'),
+        }.items() #TODO: this doesn't do anything
+    )
+
+    algorithm = IncludeLaunchDescription(
+        launch_description_source = PythonLaunchDescriptionSource(
+            os.path.join(pkg_controllers, f'{alg_launch[alg]}.launch.py')),
+        launch_arguments={
+            'car_name': TextSubstitution(text=str(config['sim']['ros__parameters']['car_name']) if 'car_name' in config['sim']['ros__parameters'] else 'f1tenth'),
+        }.items()
+    )
+
+    # Launch the Environment
+    main = Node(
+            package='controllers',
+            executable='sim',
+            parameters=[
+                config_path
+            ],
+            name='sim',
+            output='screen',
+            emulate_tty=True, # Allows python print to show
+    )
+
+    return LaunchDescription([
+        #TODO: Find a way to remove this
+        SetEnvironmentVariable(name='GZ_SIM_RESOURCE_PATH', value=pkg_f1tenth_description[:-19]),
+        SetParameter(name='use_sim_time', value=True),
+        environment,
+        algorithm,
+        main
+])
