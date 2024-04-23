2d001f3bc84098a0c12ef4259bc1aabfed4fee29
emilysteiner71 <emilysteiner71@gmail.com>
2024-04-24 11:53:22 +1200

    Runs turn and drive file

---
+++ src/controllers/controllers/turn_and_drive.py
@@ -0,0 +1,31 @@
+import rclpy
+import numpy as np
+import random
+from .controller import Controller
+
+def main():
+    rclpy.init()
+    
+    param_node = rclpy.create_node('params')
+    
+    param_node.declare_parameters(
+        '',
+        [
+            ('car_name', 'f1tenth_two'),
+        ]
+    )
+    
+    params = param_node.get_parameters(['car_name'])
+    params = [param.value for param in params]
+    CAR_NAME = params[0]
+    
+    controller = Controller('turn_drive_', CAR_NAME, 0.25)
+    policy_id = 'turn_drive'
+    
+    while True:
+        action = np.asarray([random.uniform(0, 3), random.uniform(-3.14, 3.14)])    
+        controller.step(action, policy_id)
+    
+
+if __name__ == '__main__':
+    main()
