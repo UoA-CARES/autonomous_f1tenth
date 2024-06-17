bdb830de5f19736b2effeff950af26f4a339f968
emilysteiner71 <emilysteiner71@gmail.com>
2024-06-18 10:42:18 +1200

    Defined path functions

---
+++ src/controllers/controllers/test_path.py
@@ -0,0 +1,26 @@
+def straightLine():
+    return 1
+
+def circle():
+    return 1
+
+def austinLap():
+    return 1
+
+def budapestLap():
+    return 1
+
+def hockenheimLap():
+    return 1
+
+def melbourneLap():
+    # TBC
+    return 1
+
+def saopaoloLap():
+    # TBC
+    return 1
+
+def shanghaiLap():
+    # TBC
+    return 1
