1bcf1989a2bff4053b72319e83ed3d31c8b94019
emilysteiner71 <emilysteiner71@gmail.com>
2024-06-19 14:26:53 +1200

    Straight and circular paths defined

--- src/controllers/controllers/test_path.py
+++ src/controllers/controllers/test_path.py
@@ -2,9 +2,17 @@ def straightLine():
     coordinates = ([0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0], [12, 0]) #placeholder values
     return coordinates
 
-def circle():
-    coordinates = ([0, 0], [1, 0], [3, 1], [5, 2]) #placeholder values
+def circleCW():
+    radius = 5
+    coordinates = ([0, 0], [radius, -1*radius], [0, -2*radius], [-1*radius, -1*radius]) #placeholder values
+    coordinates = ([0, 0], [radius*3/5, radius*-1/5], [radius*4/5, radius*-2/5], [radius, -radius], [radius*4/5, radius*-8/5], [radius*3/5, radius*-9/5], [0, -2*radius], [radius*-3/5, radius*-9/5], [radius*-4/5, radius*-8/5], [-1*radius, -radius], [radius*-4/5, radius*-2/5], [radius*-3/5, radius*-1/5], [0, 0])
     return coordinates
+
+def circleCCW():
+    radius = 5
+    coordinates = ([0, 0], [radius*3/5, radius*1/5], [radius*4/5, radius*2/5], [radius, radius], [radius*4/5, radius*8/5], [radius*3/5, radius*9/5], [0, 2*radius], [radius*-3/5, radius*9/5], [radius*-4/5, radius*8/5], [-1*radius, radius], [radius*-4/5, radius*2/5], [radius*-3/5, radius*1/5], [0, 0]) #placeholder values
+    return coordinates
+
 
 def austinLap():
     coordinates = ([0, 0], [1, 0], [3, 1], [5, 2]) #placeholder values
