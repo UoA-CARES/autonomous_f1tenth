9bd06ba95fc0d9bd7f882b6d7797b19a98576fec
ultron_lab_pc <ultron_lab_pc@TODO.change_this>
2024-06-19 16:10:44 +1200

    tnd & quarternion to euler util

--- src/controllers/controllers/turn_and_drive.py
+++ src/controllers/controllers/turn_and_drive.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 from rclpy.impl import rcutils_logger
 from .controller import Controller
+from environments.util import get_euler_from_quarternion
 
 
 def main():
@@ -33,8 +34,8 @@ def main():
     
     # goalx = float(state[0]) + 2
     # goaly = float(state[1]) + 2
-    goalx = 1
-    goaly = 5
+    goalx = -5
+    goaly = -2
     goal = np.asarray([goalx, goaly])
     
     while True:
@@ -51,15 +52,23 @@ class TurnAndDrive():
 
     logger = rcutils_logger.RcutilsLogger(name="tnd_log")
 
+    def __init__(self, turning_lin_vel = 0.2, turning_ang_vel_modifier = 1, straight_lin_vel = 1, angle_diff_tolerance = 0.1):
+        self.turning_lin_vel = turning_lin_vel
+        self.turning_ang_vel_modifier = turning_ang_vel_modifier
+        self.straight_lin_vel = straight_lin_vel
+        self.angle_diff_tolerance = angle_diff_tolerance
+        
+
     # Still fixing
     def select_action(self, state, goal):
         location = state[0:2]
+        self_angle = get_euler_from_quarternion(state[2],state[3],state[4],state[5])[2]
+
         self.logger.info("-------------------------------------------------")
         # self.logger.info("LOCATION: "+str(location[0])+" "+str(location[1]))
-        self.logger.info("STATE: "+str(state[0:6]))
+        self.logger.info("STATE: "+str(location)+" "+str(self_angle))
 
         
-            
         distance = goal - location
         if ((abs(distance[0]) < 0.2) and (abs(distance[1] < 0.2))):
             lin = 0
@@ -67,31 +76,30 @@ class TurnAndDrive():
             action = np.asarray([lin, ang])
             return action
         
-        angle = np.arctan2(distance[1], distance[0])
-        if (((angle - state[5]) > 0.1) or ((angle - state[5]) < -0.1)):
-            ang = angle - state[5]
-            # print(angle)
+        angle_to_goal = np.arctan2(distance[1], distance[0])
+        if (((angle_to_goal - self_angle) > self.angle_diff_tolerance) or ((angle_to_goal - self_angle) < -self.angle_diff_tolerance)):
+            ang = angle_to_goal - self_angle
             # print(state[5])
             # print(ang)
-            self.logger.info("ANGLE TO GOAL: "+str(angle))
-            self.logger.info("SELF ANGLE: "+str(state[5]))
+            self.logger.info("ANGLE TO GOAL: "+str(angle_to_goal))
+            self.logger.info("SELF ANGLE: "+str(self_angle))
             self.logger.info("DELTA: "+str(ang))
 
-            if ang > 3:
-                ang = 3
-            elif ang < -3:
-                ang = -3
+            if ang > 1.5:
+                ang = 1.5
+            elif ang < -1.5:
+                ang = -1.5
 
-            lin = 0.25
+            lin = self.turning_lin_vel
         else:
-            self.logger.info("ANGLE TO GOAL: "+str(angle))
+            self.logger.info("ANGLE TO GOAL: "+str(angle_to_goal))
             ang = 0
-            lin = 1
+            lin = self.straight_lin_vel
         
         
         self.logger.info("RESULT LIN_V: "+str(lin))
         self.logger.info("RESULT ANG_V: "+str(ang))
-        action = np.asarray([lin, ang])
+        action = np.asarray([lin, ang*self.turning_ang_vel_modifier])
         return action    
 
 if __name__ == '__main__':
