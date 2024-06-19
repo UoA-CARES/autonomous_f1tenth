4010ac38f12dd88b5bd19c949de05e53c1e89aa3
emilysteiner71 <emilysteiner71@gmail.com>
2024-06-19 13:47:01 +1200

    Cleanup

--- src/controllers/controllers/test_path.py
+++ src/controllers/controllers/test_path.py
@@ -1,5 +1,3 @@
-from collections import namedtuple
-
 def straightLine():
     coordinates = ([0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0], [12, 0]) #placeholder values
     return coordinates
