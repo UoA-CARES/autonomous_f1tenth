8167729a689b020fec50b0f341bed180a489a273
emilysteiner71 <emilysteiner71@gmail.com>
2024-07-03 10:40:11 +1200

    new turn to goal util function

--- src/environments/environments/util.py
+++ src/environments/environments/util.py
@@ -243,3 +243,32 @@ def ackermann_to_twist(delta, linear_v, L):
         print("Wheelbase must be greater than zero")
         return 0
     return omega
+
+# Returns steering angle to turn to goal
+def turn_to_goal(location, yaw, goal, goal_tolerance=0.5, angle_diff_tolerance=0.1):
+
+    distance = goal - location # x, y array
+
+    if ((abs(distance[0]) < goal_tolerance) and (abs(distance[1] < goal_tolerance))): # Already at goal
+        ang = 0
+        return ang
+     
+    angle_to_goal = np.arctan2(distance[1], distance[0])
+    if (((angle_to_goal - yaw) > angle_diff_tolerance) or ((angle_to_goal - yaw) < -angle_diff_tolerance)):
+            
+        # take the shortest turning angle
+        ang = angle_to_goal - yaw
+        if ang > np.pi:
+            ang -= 2 * np.pi
+        elif ang < -np.pi:
+            ang += 2 * np.pi
+
+
+        # make sure turning angle is not more than 90deg
+        if ang > 1.5:
+            ang = 1.5
+        elif ang < -1.5:
+            ang = -1.5
+        return ang
+    else:
+         return 0
