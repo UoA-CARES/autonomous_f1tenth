865514f8119248b812ecc451b39a6f3b45f1762d
emilysteiner71 <emilysteiner71@gmail.com>
2024-07-03 10:48:27 +1200

    Pure Pursuit uses function

--- src/controllers/controllers/pure_pursuit.py
+++ src/controllers/controllers/pure_pursuit.py
@@ -3,7 +3,7 @@ import numpy as np
 import random
 from .controller import Controller
 from rclpy.impl import rcutils_logger
-from environments.util import get_euler_from_quarternion
+from environments.util import get_euler_from_quarternion, turn_to_goal
    
 class PurePursuit():
     logger = rcutils_logger.RcutilsLogger(name="pure_pursuit_log")
@@ -31,28 +31,7 @@ class PurePursuit():
 
         # Calculate angle same as turn and drive
         trueGoal = self.findGoal(location, yawcurr)
-        distance = trueGoal - location
-        angle_to_goal = np.arctan2(distance[1], distance[0])
-        if (((angle_to_goal - yawcurr) > self.angle_diff_tolerance) or ((angle_to_goal - yawcurr) < -self.angle_diff_tolerance)):
-            
-            # take the shortest turning angle
-            self.steering_ang = angle_to_goal - yawcurr
-            if self.steering_ang > np.pi:
-                self.steering_ang -= 2 * np.pi
-            elif self.steering_ang < -np.pi:
-                self.steering_ang += 2 * np.pi
-
-            self.logger.info("ANGLE TO GOAL: "+str(angle_to_goal))
-            self.logger.info("SELF ANGLE: "+str(yawcurr))
-            self.logger.info("DELTA: "+str(self.steering_ang))
-
-            # make sure turning angle is not more than 90deg
-            if self.steering_ang > 1.5:
-                self.steering_ang = 1.5
-            elif self.steering_ang < -1.5:
-                self.steering_ang = -1.5
-
-        ang = random.uniform(-3.14, 3.14)
+        ang = turn_to_goal(location, yawcurr, goal)
         action = np.asarray([lin, ang])
         self.logger.info("DRIVE LIN_V: "+str(action[0]))
         self.logger.info("DRIVE ANGLE: "+str(action[1]))
