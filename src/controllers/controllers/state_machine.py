34c3ba3fcf966b46594cee33abe36221caae197a
emilysteiner71 <emilysteiner71@gmail.com>
2024-09-10 14:03:45 +1200

    Detects when lap completed

--- src/controllers/controllers/state_machine.py
+++ src/controllers/controllers/state_machine.py
@@ -1,38 +1,28 @@
 import rclpy
 from rclpy.node import Node
+from rclpy import Future
 from std_msgs.msg import String
 import time
 from message_filters import Subscriber
 from nav_msgs.msg import Odometry
 from .util import absoluteDistance
 import numpy as np
+from .controller import Controller
 
 class StateMachine(Node):
     def __init__(self):
         super().__init__('state_machine')
+        
+        self.init_odom = []
+        self.odom = []
 
         self.state_pub = self.create_publisher(
             String,
             '/state',
             10
         )
+        self.odomController = Controller("odom_controller", 'f1tenth', 0.1)
 
-        self.odom_sub = self.create_subscription(
-            Odometry,
-            '/f1tenth/odometry',
-            self.odom_callback,
-            10
-        )   
-    
-        self.currState = 'setup'
-        self.odom = []
-        self.init_odom = []
-
-    def odom_callback(self, msg):
-        position = msg.pose.pose.position
-        self.odom = [position.x, position.y]
-        #self.get_logger().info("reading odom")
-    
     def pubState(self, str):
         msg = String()
         msg.data = str
@@ -44,23 +34,40 @@ class StateMachine(Node):
     def changeState(self, newState):
         self.currState = newState
 
+    def getOdom(self):
+        state = self.odomController.get_observation("stateMachine")
+        return state[0:2]
+
 def main():
     rclpy.init()
     state_machine = StateMachine()
     print("In state machine")
+
     # Get initial odometry
+    time.sleep(0.2)
     while(len(state_machine.odom) == 0):
-        rclpy.spin_once(state_machine)
-        state_machine.get_logger().info(str(state_machine.odom))
+        state_machine.odom = state_machine.getOdom()
         time.sleep(2)
     state_machine.init_odom = state_machine.odom
-
+    state_machine.get_logger().info(str(state_machine.init_odom))
     #Waiting to move
+    state_machine.odom = state_machine.getOdom()
     while (absoluteDistance(np.array(state_machine.init_odom), np.array(state_machine.odom)) < 0.2):
-        rclpy.spin_once(state_machine)
+        state_machine.odom = state_machine.getOdom()
         time.sleep(0.1)
     state_machine.changeState("mapping")
-
+    state_machine.get_logger().info("Begin mapping")
+    time.sleep(5)
+    state_machine.odom = state_machine.getOdom()
+    while (absoluteDistance(np.array(state_machine.init_odom), np.array(state_machine.odom)) > 0.5):
+        stringToPrint = "Initial odom" + str(state_machine.init_odom) + " , current odom: " + str(state_machine.odom) + ", distance: " + str(absoluteDistance(np.array(state_machine.init_odom), np.array(state_machine.odom)))
+        state_machine.get_logger().info(stringToPrint) 
+        state_machine.odom = state_machine.getOdom()
+        time.sleep(0.1)
+    stringToPrint = "Initial odom" + str(state_machine.init_odom) + " , current odom: " + str(state_machine.odom) + ", distance: " + str(absoluteDistance(np.array(state_machine.init_odom), np.array(state_machine.odom)))
+    state_machine.get_logger().info(stringToPrint)   
+    state_machine.get_logger().info("One lap completed")
+    state_machine.changeState("planning")
 
     state_machine.destroy_node()
     rclpy.shutdown()
