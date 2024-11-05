8132763666c91e796f82f4ab72d9dffa1b3d3c6c
emilysteiner71 <emilysteiner71@gmail.com>
2024-11-05 14:13:15 +1300

    Sorted tracking

--- src/controllers/launch/sim.launch.py
+++ src/controllers/launch/sim.launch.py
@@ -28,7 +28,7 @@ def generate_launch_description():
     pkg_f1tenth_description = get_package_share_directory('f1tenth_description')
     pkg_environments = get_package_share_directory('environments')
     pkg_controllers = get_package_share_directory('controllers')
-    pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
+    #pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
     pkg_slam = get_package_share_directory('slam_toolbox')
 
     config_path = os.path.join(
@@ -39,10 +39,23 @@ def generate_launch_description():
     config = yaml.load(open(config_path), Loader=yaml.Loader)
     env = config['sim']['ros__parameters']['environment']
     alg = config['sim']['ros__parameters']['algorithm']
-    if ((alg == 'turn_and_drive') or (alg == 'mpc') or (alg == 'random') or (alg == 'pure_pursuit')):
-        tracking = True
-    else:
-        tracking = False
+    ispreplanned = config['sim']['ros__parameters']['preplan']
+
+
+    match alg:
+        case 'rl':
+            tracking = False
+        case 'ftg':
+            tracking = False
+        case 'random':
+            tracking = True
+        case 'turn_drive':
+            tracking = True
+        case 'mpc':
+            tracking = True
+        case 'pure_pursuit':
+            tracking = True
+
 
     environment =  IncludeLaunchDescription(
         launch_description_source=PythonLaunchDescriptionSource(
@@ -68,69 +81,84 @@ def generate_launch_description():
             emulate_tty=True, # Allows python print to show
     )
 
-    # state_machine = Node(
-    #     package='controllers',
-    #     executable= 'state_machine',
-    #     output='screen',
-    #     emulate_tty=True,
-    # )
+    
 
 
     if tracking:
-        alg = Node(
-        package='controllers',
-        executable='track',
-        output='screen',
-        parameters=[{'car_name': TextSubstitution(text=str(config['sim']['ros__parameters']['car_name']) if 'car_name' in config['sim']['ros__parameters'] else 'f1tenth')},
-                    {'alg': TextSubstitution(text=str(alg))}, {'path_file_path': TextSubstitution(text=str(config['sim']['ros__parameters']['path_file_path']))}],
-        )
-        # TF TREE: map ------------ odom --------------- (baselink--lidar_link)
-        # https://www.youtube.com/watch?v=ZaiA3hWaRzE
-        # Launch static transform node to provide TF info for baselink to lidar_link (sticking them together cause it is not known somehow)
-        lidar_to_base_tf_node = launch_ros.actions.Node( 
-            package='tf2_ros', 
-            executable='static_transform_publisher', 
-            arguments=['0', '0', '0', '0', '0', '0', "f1tenthbase_link", "f1tenthhokuyo_10lx_lidar_link"], 
-            output='screen'
-        )
-        # Launch odom localization node to provide TF info from odom reading to base_link
-        odom_to_base_tf_node = launch_ros.actions.Node(
-            package='robot_localization',
-            executable='ekf_node',
-            name='ekf_filter_node',
+        state_machine = Node(
+            package='controllers',
+            executable= 'state_machine',
             output='screen',
-            parameters=[os.path.join(pkg_f1tenth_description, 'config/ekf.yaml'), {'use_sim_time': True}]
+            emulate_tty=True,
         )
 
-        slam_node = IncludeLaunchDescription(
-        launch_description_source = os.path.join(pkg_slam,f'launch/online_async_launch.py'),
-        launch_arguments = {
-            'use_sim_time': 'True',
-            'slam_params_file':"./src/f1tenth/f1tenth_description/config/slam_toolbox.yaml"
-        }.items() 
+        alg = Node(
+            package='controllers',
+            executable='track',
+            output='screen',
+            parameters=[{'car_name': TextSubstitution(text=str(config['sim']['ros__parameters']['car_name']) if 'car_name' in config['sim']['ros__parameters'] else 'f1tenth')},
+                        {'alg': TextSubstitution(text=str(alg))}, {'path_file_path': TextSubstitution(text=str(config['sim']['ros__parameters']['path_file_path']))}],
         )
 
-        # ftg_node = Node(
-        #         package='controllers',
-        #         executable='ftg_policy',
-        #         output='screen',
-        #         parameters=[{'car_name': TextSubstitution(text=str(config['sim']['ros__parameters']['car_name']) if 'car_name' in config['sim']['ros__parameters'] else 'f1tenth')}],
-        # )
-        
+        if ispreplanned:
 
-        return LaunchDescription([
+            return LaunchDescription([
             SetEnvironmentVariable(name='GZ_SIM_RESOURCE_PATH', value=pkg_f1tenth_description[:-19]),
             SetParameter(name='use_sim_time', value=True),
             environment,
             alg,
             sim,
-            lidar_to_base_tf_node,
-            odom_to_base_tf_node,
-            slam_node,
-            #state_machine,
-            #ftg_node
-            #algorithm
-        ])
+            state_machine,
+            ])
+        
+        else:
+            
+            # TF TREE: map ------------ odom --------------- (baselink--lidar_link)
+            # https://www.youtube.com/watch?v=ZaiA3hWaRzE
+            # Launch static transform node to provide TF info for baselink to lidar_link (sticking them together cause it is not known somehow)
+            lidar_to_base_tf_node = launch_ros.actions.Node( 
+                package='tf2_ros', 
+                executable='static_transform_publisher', 
+                arguments=['0', '0', '0', '0', '0', '0', "f1tenthbase_link", "f1tenthhokuyo_10lx_lidar_link"], 
+                output='screen'
+            )
+            # Launch odom localization node to provide TF info from odom reading to base_link
+            odom_to_base_tf_node = launch_ros.actions.Node(
+                package='robot_localization',
+                executable='ekf_node',
+                name='ekf_filter_node',
+                output='screen',
+                parameters=[os.path.join(pkg_f1tenth_description, 'config/ekf.yaml'), {'use_sim_time': True}]
+            )
+
+            slam_node = IncludeLaunchDescription(
+            launch_description_source = os.path.join(pkg_slam,f'launch/online_async_launch.py'),
+            launch_arguments = {
+                'use_sim_time': 'True',
+                'slam_params_file':"./src/f1tenth/f1tenth_description/config/slam_toolbox.yaml"
+            }.items() 
+            )
+
+            ftg_node = Node(
+                    package='controllers',
+                    executable='ftg_policy',
+                    output='screen',
+                    parameters=[{'car_name': TextSubstitution(text=str(config['sim']['ros__parameters']['car_name']) if 'car_name' in config['sim']['ros__parameters'] else 'f1tenth')}],
+            )
+            
+
+            return LaunchDescription([
+                SetEnvironmentVariable(name='GZ_SIM_RESOURCE_PATH', value=pkg_f1tenth_description[:-19]),
+                SetParameter(name='use_sim_time', value=True),
+                environment,
+                alg,
+                sim,
+                lidar_to_base_tf_node,
+                odom_to_base_tf_node,
+                slam_node,
+                state_machine,
+                ftg_node
+            ])
 
     else:
         if (f'{alg}' != 'rl'):
