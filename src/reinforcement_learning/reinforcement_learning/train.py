11897c65e72e21c2e9dba32802d4fe563a9933c8
emilysteiner71 <emilysteiner71@gmail.com>
2025-08-26 15:18:04 +1200

    second env

--- src/reinforcement_learning/reinforcement_learning/train.py
+++ src/reinforcement_learning/reinforcement_learning/train.py
@@ -41,6 +41,10 @@ def main():
     network_factory = NetworkFactory()
 
     env = env_factory.create(env_config['environment'], env_config)
+    if env_config['environment'] == 'MultiAgent':
+        env2_config = env_config
+        env2_config['car_name'] = 'f2tenth'
+        env2 = env_factory.create(env_config['environment'], env2_config)
     agent = network_factory.create_network(env.OBSERVATION_SIZE, env.ACTION_NUM, config=network_config)
     memory = MemoryBuffer(algorithm_config['buffer_size'])
 
