21b7b9c494753c2763ae3593bdde9e69ced88e57
F Z <futianzhou@hotmail.com>
2025-09-12 16:41:24 +1200

    update

--- src/controllers/controllers/rl_policy.py
+++ src/controllers/controllers/rl_policy.py
@@ -6,16 +6,17 @@ from cares_reinforcement_learning.util.helpers import denormalize
 from cares_reinforcement_learning.util.network_factory import NetworkFactory
 from reinforcement_learning.parse_args import parse_args
 import yaml
-from pathlib import Path
 import os
-from datetime import datetime
-import time
 
 def main():
     rclpy.init()
+    ament_path = os.environ["AMENT_PREFIX_PATH"].split(":")[0]
+    workspace_dir = os.path.dirname(ament_path)
+    car_path = os.path.join(workspace_dir, "..", "src", "controllers", "config")
+    config_path = os.path.join(workspace_dir, "..", "src", "environments", "config")
     
     # Load configuration from YAML file
-    with open('/home/anyone/new_repo/autonomous_f1tenth/src/controllers/config/car.yaml', 'r') as file:
+    with open(f'{car_path}/car.yaml', 'r') as file:
         config = yaml.safe_load(file)
 
     env_config, _, network_config, rest = parse_args()
@@ -39,31 +40,20 @@ def main():
     else:
         raise Exception('Both actor and critic paths must be provided')
     
-    ament_path = os.environ["AMENT_PREFIX_PATH"].split(":")[0]
-    workspace_dir = os.path.dirname(ament_path)
-    path = os.path.join(workspace_dir, "..", "recordings", "network_outputs")
-    if not os.path.exists(path):
-         os.mkdir(path)
-    filepath = os.path.join(path, f"network_output_{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.csv")
-    with open(filepath, 'w') as f:
-        f.write("time,speed,steering\n")
         
     state = controller.step([0, 0], policy_id)
     # Full state is [coordinate_x, coordinate_y, orientation_x, orientation_y, orientation_z, orientation_w, linear_velocity, angular_velocity, 10 x lidar_data]
     # 'lidar_only' only considers linear_velocity and angular_velocity, thus the first 6 elements are omitted
     state = state[6:]
 
-    with open('/home/anyone/new_repo/autonomous_f1tenth/src/environments/config/config.yaml', 'r') as file:
-            actions_config = yaml.safe_load(file)
-    max_actions = np.asarray([actions_config['actions']['max_speed'], actions_config['actions']['max_turn']])
-    min_actions = np.asarray([actions_config['actions']['min_speed'], actions_config['actions']['min_turn']])
+    with open(f'{config_path}/config.yaml', 'r') as file:
+        actions_config = yaml.safe_load(file)
+    MAX_CONFIG_ACTIONS = np.asarray([actions_config['actions']['max_speed'], actions_config['actions']['max_turn']])
+    MIN_CONFIG_ACTIONS = np.asarray([actions_config['actions']['min_speed'], actions_config['actions']['min_turn']])
+    
     while True:
         action = agent.select_action_from_policy(state)
-        timestamp = time.time()
-        with open(filepath, 'a') as f:
-                f.write(f"{timestamp},{action[0]:.4f},{action[1]:.4f}\n")
-        
-        action = denormalize(action, max_actions, min_actions)
+        action = denormalize(action, MAX_CONFIG_ACTIONS, MIN_CONFIG_ACTIONS)
         action = np.clip(action, MIN_ACTIONS, MAX_ACTIONS)
         state = controller.step(action, policy_id)
         state = state[6:]
