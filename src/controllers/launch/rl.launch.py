a568f92781ffc7eb5d31171c2da4cc1754be75d3
emilysteiner71 <emilysteiner71@gmail.com>
2026-01-21 12:19:43 +1300

    controller launch file tidying

--- src/controllers/launch/rl.launch.py
+++ src/controllers/launch/rl.launch.py
@@ -4,7 +4,6 @@ from launch_ros.actions import Node
 from ament_index_python import get_package_share_directory
 
 def generate_launch_description():
-
     config_path = os.path.join(
         get_package_share_directory('controllers'),
         'rl_policy.yaml'
@@ -15,9 +14,7 @@ def generate_launch_description():
         executable='rl_policy',
         output='screen',
         name='rl_policy',
-        parameters=[
-            config_path
-        ],
+        parameters=[config_path],
     )
     
     vel_recorder = Node(
