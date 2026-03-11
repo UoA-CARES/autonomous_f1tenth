0b32b79d79ff163e10608718be240bd963f44c58
beardyface <henryamwilliams@gmail.com>
2026-03-11 15:47:07 +1300

    state correction

--- src/environments/environments/CarTrackEnvironment.py
+++ src/environments/environments/CarTrackEnvironment.py
@@ -293,6 +293,9 @@ class CarTrackEnvironment(F1tenthEnvironment):
 
         if self.IS_EVAL and (terminated or truncated):
             self.EVAL_TRACK_IDX
+
+        print(f"{full_next_state=}")
+        print(f"{full_next_state.shape=}")
         return full_next_state, reward, terminated, truncated, info
 
     def is_terminated(self, state, ranges):
