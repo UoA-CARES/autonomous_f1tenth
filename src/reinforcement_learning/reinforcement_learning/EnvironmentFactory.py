9cc14536e43af34775ed8eed1532918812bc2777
beardyface <henryamwilliams@gmail.com>
2026-03-11 13:50:51 +1300

    Factory Configuration

--- src/reinforcement_learning/reinforcement_learning/EnvironmentFactory.py
+++ src/reinforcement_learning/reinforcement_learning/EnvironmentFactory.py
@@ -5,85 +5,86 @@ from environments.CarOvertakeEnvironment import CarOvertakeEnvironment
 from environments.TwoCarEnvironment import TwoCarEnvironment
 from environments.MultiAgentEnvironment import MultiAgentEnvironment
 
+
 class EnvironmentFactory:
     def __init__(self):
         pass
 
     def create(self, name, config):
         print(config)
-        if name == 'CarTrack':
+        if name == "CarTrack":
             return CarTrackEnvironment(
-                config['car_name'], 
-                config['reward_range'], 
-                config['max_steps'], 
-                config['collision_range'], 
-                config['step_length'], 
-                config['track'], 
-                config['observation_mode'], 
-                config['is_staged_training']
-                )
-        elif name == 'CarRace':
+                # config["car_name"],
+                # config["reward_range"],
+                # config["max_steps"],
+                # config["collision_range"],
+                # config["step_length"],
+                # config["track"],
+                # config["observation_mode"],
+                # config["is_staged_training"],
+            )
+        elif name == "CarRace":
             return CarRaceEnvironment(
-                config['car_name'], 
-                config['reward_range'], 
-                config['max_steps'], 
-                config['collision_range'], 
-                config['step_length'], 
-                config['track'], 
-                config['observation_mode'], 
-                )
-        elif name == 'CarOvertake':
+                config["car_name"],
+                config["reward_range"],
+                config["max_steps"],
+                config["collision_range"],
+                config["step_length"],
+                config["track"],
+                config["observation_mode"],
+            )
+        elif name == "CarOvertake":
             return CarOvertakeEnvironment(
-                config['car_name'], 
-                config['reward_range'], 
-                config['max_steps'], 
-                config['collision_range'], 
-                config['step_length'], 
-                config['track'], 
-                config['observation_mode'], 
+                config["car_name"],
+                config["reward_range"],
+                config["max_steps"],
+                config["collision_range"],
+                config["step_length"],
+                config["track"],
+                config["observation_mode"],
             )
-        elif name == 'TwoCar':
+        elif name == "TwoCar":
             return TwoCarEnvironment(
-                config['car_name'], 
-                config['reward_range'], 
-                config['max_steps'], 
-                config['collision_range'], 
-                config['step_length'], 
-                config['track'], 
-                config['observation_mode'], 
+                config["car_name"],
+                config["reward_range"],
+                config["max_steps"],
+                config["collision_range"],
+                config["step_length"],
+                config["track"],
+                config["observation_mode"],
             )
-        elif name == 'CarBeat':
+        elif name == "CarBeat":
             return CarBeatEnvironment(
-                config['car_name'],
-                config['ftg_car_name'], 
-                config['reward_range'], 
-                config['max_steps'], 
-                config['collision_range'], 
-                config['step_length'], 
-                config['track'], 
-                config['observation_mode'], 
-                config['max_goals'],
-                config['num_lidar_points']
+                config["car_name"],
+                config["ftg_car_name"],
+                config["reward_range"],
+                config["max_steps"],
+                config["collision_range"],
+                config["step_length"],
+                config["track"],
+                config["observation_mode"],
+                config["max_goals"],
+                config["num_lidar_points"],
             )
-        elif name == 'MultiAgent':
+        elif name == "MultiAgent":
             return MultiAgentEnvironment(
-                config['car_name'], 
-                config['reward_range'], 
-                config['max_steps'], 
-                config['collision_range'], 
-                config['step_length'], 
-                config['track'], 
-                config['observation_mode'], 
+                config["car_name"],
+                config["reward_range"],
+                config["max_steps"],
+                config["collision_range"],
+                config["step_length"],
+                config["track"],
+                config["observation_mode"],
             )
-        elif name == 'MultiAgent2':
+        elif name == "MultiAgent2":
             return MultiAgentEnvironment(
-                'f2tenth', 
-                config['reward_range'], 
-                config['max_steps'], 
-                config['collision_range'], 
-                config['step_length'], 
-                config['track'], 
-                config['observation_mode'], 
+                "f2tenth",
+                config["reward_range"],
+                config["max_steps"],
+                config["collision_range"],
+                config["step_length"],
+                config["track"],
+                config["observation_mode"],
             )
         else:
-            raise Exception('EnvironmentFactory: Environment not found')
+            raise Exception("EnvironmentFactory: Environment not found")
