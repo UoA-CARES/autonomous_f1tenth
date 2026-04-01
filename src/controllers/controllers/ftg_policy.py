0984f9ca1939b61a6bd35b6335534db49f374f9d
beardyface <henryamwilliams@gmail.com>
2026-04-01 16:19:14 +1300

    ftg2

--- src/controllers/controllers/ftg_policy.py
+++ src/controllers/controllers/ftg_policy.py
@@ -24,6 +24,7 @@ def main():
 
     while rclpy.ok():
         action = policy.select_action(state)
+        controller.get_logger().info(f"Selected action: {action}")
         state = controller.step(action, policy_id)
 
     rclpy.shutdown()
