262bf04676c267beceafcaa63cb367a282483bb0
beardyface <henryamwilliams@gmail.com>
2026-04-17 11:21:48 +1200

    README

---
+++ autonomous_bringup/launch/rviz.launch.py
@@ -0,0 +1,21 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    rviz_config_path = os.path.join(
+        get_package_share_directory("autonomous_bringup"), "rviz", "f1tenth.rviz"
+    )
+    return LaunchDescription(
+        [
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                output="screen",
+                arguments=["-d", rviz_config_path],
+            ),
+        ]
+    )
